434365782330e065ff3e5d50e7360f5b24d54572
tudya <tudya1238@gmail.com>
2023-09-05 09:59:09 +0500

    add to cart + token

--- src/useSessionToken.ts
+++ src/useSessionToken.ts
@@ -1,12 +1,27 @@
 import axios from "axios";
 
-let sessionToken: string | null = null;
+// Функция для сохранения токена в localStorage
+function setSessionToken(token: string) {
+    localStorage.setItem("sessionToken", token);
+}
+
+// Функция для получения токена из localStorage
+function getSessionToken() {
+    return localStorage.getItem("sessionToken");
+}
+
+let sessionToken: string | null = getSessionToken() || null;
 
 async function generateSessionToken() {
     try {
+        console.log('Запрос на генерацию токена сессии отправлен');
         const response = await axios.post<{ sessionToken: string }>('https://brand-outlet.shop/api/order/create-session');
-        sessionToken = response.data.sessionToken; // Предположим, что сервер возвращает токен в поле sessionToken
-        console.log(sessionToken);
+        sessionToken = response.data.sessionToken;
+
+        // Сохраняем токен в localStorage
+        setSessionToken(sessionToken);
+        console.log('Сессионный токен получен:', sessionToken);
+        console.log(localStorage)
     } catch (error) {
         console.error('Ошибка при генерации токена сессии:', error);
     }
