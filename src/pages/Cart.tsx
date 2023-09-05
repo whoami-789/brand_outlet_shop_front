4f27e8ac1e8cc3e7344013ad0c2614acc12a55f9
tudya <tudya1238@gmail.com>
2023-09-05 12:06:39 +0500

    add to cart + token

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,9 +1,9 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {Cart} from "../components/Cart";
-import {Button} from "@mui/material";
-import {Link} from "react-router-dom";
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Cart } from "../components/Cart";
+import { Button, TextField } from "@mui/material";
+import { Link } from "react-router-dom";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 interface Product {
     id: number;
@@ -15,16 +15,26 @@ interface Product {
 export function Cart_page() {
     const [products, setProducts] = useState<Product[]>([]);
     const [totalPrice, setTotalPrice] = useState<number>(0);
+    const [telegramFeed, setTelegramFeed] = useState<string>("");
 
     useEffect(() => {
-        axios.get<Product[]>("https://fakestoreapi.com/products")
-            .then(response => {
-                setProducts(response.data);
-            })
-            .catch(error => {
-                console.error("Ошибка при получении данных о продуктах:", error);
-            });
+        // Здесь вы можете добавить логику для отправки запроса на получение данных корзины с использованием токена сессии из localStorage
+
+        const sessionToken = localStorage.getItem("sessionToken");
+
+        if (sessionToken) {
+            axios
+                .get(`https://brand-outlet.shop/api/cart/view?sessionToken=${sessionToken}`)
+                .then((response) => {
+                   console.log(response.data);                    // Обновите состояние с полученными данными корзины
+                    // Например, setCartItems(cartItems);
+                })
+                .catch((error) => {
+                    console.error("Ошибка при получении данных корзины:", error);
+                });
+        }
     }, []);
+
 
     useEffect(() => {
         const calculatedTotalPrice = products.reduce((total, product) => {
@@ -35,25 +45,33 @@ export function Cart_page() {
 
     return (
         <div className="bg-gray-300 w-full mr-2">
-
-            {/* Кнопка возврата на предыдущую страницу */}
-            <Link to="/" className="mb-3 mt-3 ml-5 flex items-center text-gray-700 hover:underline">
+            <Link
+                to="/"
+                className="mb-3 mt-3 ml-5 flex items-center text-gray-700 hover:underline"
+            >
                 <div className="w-6 h-6 bg-white rounded-full flex items-center justify-center mr-2">
                     <ArrowBackIcon />
                 </div>
             </Link>
-
-            <div
-                className="mt-2 inline-block w-full max-h-[calc(100vh-100px)] overflow-y-auto scrollbar-hide overflow-hidden"
-            >
+            <div className="mt-2 inline-block w-full max-h-[calc(100vh-100px)] overflow-y-auto scrollbar-hide overflow-hidden">
                 {products.map((product) => (
-                    <Cart key={product.id} product={product}/>
+                    <Cart key={product.id} product={product} />
                 ))}
             </div>
             <div className="flex ml-16">
                 <p className="font-bold text-lg w-44">Общая стоимость:</p>
-                <p className="font-bold text-lg w-40">{totalPrice.toFixed(2)} ₽</p>
+                <p className="font-bold text-lg w-40">
+                    {totalPrice.toFixed(2)} ₽
+                </p>
             </div>
+            <TextField
+                label="Telegram-фид"
+                variant="outlined"
+                fullWidth
+                value={telegramFeed}
+                onChange={(e) => setTelegramFeed(e.target.value)}
+                className="mb-3"
+            />
             <Button
                 sx={{
                     mb: 3,
@@ -61,8 +79,8 @@ export function Cart_page() {
                     ml: 10,
                     minWidth: 220,
                     height: 30,
-                    backgroundColor: '#949494',
-                    '&:hover': {
+                    backgroundColor: "#949494",
+                    "&:hover": {
                         background: "#767676",
                     },
                 }}
