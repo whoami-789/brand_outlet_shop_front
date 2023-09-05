edd6bba4d55de49bd2f37ba592a86025ad629126
tudya <tudya1238@gmail.com>
2023-09-05 10:13:07 +0500

    add to cart + token

--- src/useSessionToken.ts
+++ src/useSessionToken.ts
@@ -1,23 +1,33 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-// Инициализация токена пустой строкой
-const initialSessionToken = "";
+const SESSION_TOKEN_KEY = "sessionToken"; // Ключ для хранения токена в локальном хранилище
 
 export function useSessionToken() {
     // Используйте useState для управления токеном
-    const [sessionToken, setSessionToken] = useState(initialSessionToken);
+    const [sessionToken, setSessionToken] = useState<string | null>(null);
 
     // Функция для генерации токена
     async function generateSessionToken() {
         try {
-            console.log('Запрос на генерацию токена сессии отправлен');
-            const response = await axios.post<{ sessionToken: string }>('https://brand-outlet.shop/api/order/create-session');
-            const newSessionToken = response.data.sessionToken;
-            console.log('Сессионный токен получен:', newSessionToken);
+            // Проверяем, есть ли токен в локальном хранилище
+            const storedSessionToken = localStorage.getItem(SESSION_TOKEN_KEY);
 
-            // Обновите состояние с полученным токеном
-            setSessionToken(newSessionToken);
+            if (storedSessionToken) {
+                console.log('Сессионный токен взят из локального хранилища:', storedSessionToken);
+                setSessionToken(storedSessionToken);
+            } else {
+                console.log('Запрос на генерацию токена сессии отправлен');
+                const response = await axios.post<{ sessionToken: string }>('https://brand-outlet.shop/api/order/create-session');
+                const newSessionToken = response.data.sessionToken;
+                console.log('Сессионный токен получен:', newSessionToken);
+
+                // Сохраняем токен в локальное хранилище
+                localStorage.setItem(SESSION_TOKEN_KEY, newSessionToken);
+
+                // Обновите состояние с полученным токеном
+                setSessionToken(newSessionToken);
+            }
         } catch (error) {
             console.error('Ошибка при генерации токена сессии:', error);
         }
