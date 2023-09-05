c7318d8d785761647de6bc85294e3e27916bfb1e
tudya <tudya1238@gmail.com>
2023-09-05 13:30:54 +0500

    cart items

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import {Avatar, Box, Button} from "@mui/material";
+import React, { useState } from "react";
+import { Avatar, Box, Button } from "@mui/material";
 import photo from "../images/1.jpg";
 
 interface CartProps {
@@ -11,8 +11,6 @@ interface CartProps {
         sizes: any[]; // Здесь нужно определить структуру данных для sizes, если это массив
         categoryName: string | null;
     };
-}
-interface SizesProps {
     productSize: {
         id: number;
         size: string;
@@ -22,7 +20,7 @@ interface SizesProps {
     };
 }
 
-export function Cart({product}: CartProps, {productSize}: SizesProps) {
+export function Cart({ product, productSize }: CartProps) {
     const [quantity, setQuantity] = useState(1);
 
     const handleDecrease = () => {
@@ -36,83 +34,81 @@ export function Cart({product}: CartProps, {productSize}: SizesProps) {
     };
 
     return (
-        <>
-            <div>
-                <Box
-                    sx={{
-                        width: 350,
-                        height: 100,
-                        backgroundColor: 'primary.white',
-                    }}
-                >
-                    <div className="flex">
-                        <div
+        <div>
+            <Box
+                sx={{
+                    width: 350,
+                    height: 100,
+                    backgroundColor: 'primary.white',
+                }}
+            >
+                <div className="flex">
+                    <div
+                        style={{
+                            width: 150,
+                            height: 90,
+                            marginLeft: 3,
+                            position: 'relative', // Добавлено для позиционирования
+                        }}
+                    >
+                        <img
+                            src={product.img1}
+                            alt={product.title}
                             style={{
-                                width: 150,
-                                height: 90,
-                                marginLeft: 3,
-                                position: 'relative', // Добавлено для позиционирования
+                                width: '100%',
+                                height: '100%',
+                                objectFit: 'cover',
+                            }}
+                        />
+                    </div>
+                    <div className="ml-4 w-48">
+                        <p className="text-sm w-48 truncate">{product.title}</p>
+                        <p className="font-bold text-sm w-40">Размер: {productSize.size}</p>
+                        <p className="font-bold text-sm w-40">{productSize.priceRub} ₽</p>
+                        <Box
+                            sx={{
+                                borderRadius: 1.5,
+                                width: 100,
+                                height: 30,
+                                backgroundColor: '#949494',
+                                display: 'flex',
                             }}
                         >
-                            <img
-                                src={product.img1}
-                                alt={product.title}
-                                style={{
-                                    width: '100%',
-                                    height: '100%',
-                                    objectFit: 'cover',
-                                }}
-                            />
-                        </div>
-                        <div className="ml-4 w-48">
-                            <p className="text-sm w-48 truncate">{product.title}</p>
-                            <p className="font-bold text-sm w-40">Размер: {productSize.size}</p>
-                            <p className="font-bold text-sm w-40">{productSize.priceRub} ₽</p>
-                            <Box
+                            <Button
+                                onClick={handleDecrease}
                                 sx={{
-                                    borderRadius: 1.5,
-                                    width: 100,
+                                    color: '#ffff',
+                                    minWidth: 45,
                                     height: 30,
-                                    backgroundColor: '#949494',
-                                    display: 'flex',
+                                    '&:hover': {
+                                        background: "#767676",
+                                    },
                                 }}
+                                variant="text"
                             >
-                                <Button
-                                    onClick={handleDecrease}
-                                    sx={{
-                                        color: '#ffff',
-                                        minWidth: 45,
-                                        height: 30,
-                                        '&:hover': {
-                                            background: "#767676",
-                                        },
-                                    }}
-                                    variant="text"
-                                >
-                                    -
-                                </Button>
-                                <p className="text-white pt-0.5" id="num">
-                                    {quantity}
-                                </p>
-                                <Button
-                                    onClick={handleIncrease}
-                                    sx={{
-                                        color: '#ffff',
-                                        minWidth: 45,
-                                        height: 30,
-                                        '&:hover': {
-                                            background: "#767676",
-                                        },
-                                    }}
-                                    variant="text"
-                                >
-                                    +
-                                </Button>
-                            </Box>
-                        </div>
+                                -
+                            </Button>
+                            <p className="text-white pt-0.5" id="num">
+                                {quantity}
+                            </p>
+                            <Button
+                                onClick={handleIncrease}
+                                sx={{
+                                    color: '#ffff',
+                                    minWidth: 45,
+                                    height: 30,
+                                    '&:hover': {
+                                        background: "#767676",
+                                    },
+                                }}
+                                variant="text"
+                            >
+                                +
+                            </Button>
+                        </Box>
                     </div>
-                </Box>
-            </div>
-        </>
+                </div>
+            </Box>
+        </div>
     )
 }
