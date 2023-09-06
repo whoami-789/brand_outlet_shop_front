b76e11a534745e86c651b13e5e112306b05db2ed
tudya <tudya1238@gmail.com>
2023-09-06 14:07:50 +0500

    checkout

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -109,52 +109,59 @@ export function Cart_page() {
                     <ArrowBackIcon />
                 </div>
             </Link>
-            <div className="mt-2 inline-block w-full max-h-[calc(100vh-100px)] overflow-y-auto scrollbar-hide overflow-hidden">
-                {orderPlaced ? null : ( // Если заказ оформлен, скрываем этот блок
-                    products.map((cartItem) => (
-                        <Cart
-                            key={cartItem.product.id}
-                            product={cartItem.product}
-                            productSize={cartItem.productSize}
-                            quantity={cartItem.quantity}
-                            updateQuantity={(newQuantity) => {
-                                updateCartItemQuantity(cartItem.cartItemId, newQuantity);
-                            }}
-                        />
-                    ))
-                )}
-            </div>
-            <div className="flex ml-16">
-                <p className="font-bold text-lg w-44">Общая стоимость:</p>
-                <p className="font-bold text-lg w-40">
-                    {totalPrice.toFixed(2)} ₽
+            {orderPlaced ? ( // Отображаем благодарность после оформления заказа
+                <p className="thank-you-text text-green-500 font-bold text-xl mt-4 ml-5">
+                    Спасибо за заказ
                 </p>
-            </div>
-            <div className="mb-2 ml-20">
-                <TextField
-                    label="Ваш TelegramID"
-                    variant="outlined"
-                    value={telegramFeed}
-                    onChange={handleTelegramIdChange}
-                    size="small"
-                />
-            </div>
-
-            <div>
-                {orderPlaced ? ( // Отображаем благодарность после оформления заказа
-                    <p className="thank-you-text text-green-500 font-bold text-xl mt-4">
-                        Спасибо за заказ
-                    </p>
-                ) : (
+            ) : (
+                <div>
+                    <div className="mt-2 inline-block w-full max-h-[calc(100vh-100px)] overflow-y-auto scrollbar-hide overflow-hidden">
+                        {products.map((cartItem) => (
+                            <Cart
+                                key={cartItem.product.id}
+                                product={cartItem.product}
+                                productSize={cartItem.productSize}
+                                quantity={cartItem.quantity}
+                                updateQuantity={(newQuantity) => {
+                                    updateCartItemQuantity(cartItem.cartItemId, newQuantity);
+                                }}
+                            />
+                        ))}
+                    </div>
+                    <div className="flex ml-16">
+                        <p className="font-bold text-lg w-44">Общая стоимость:</p>
+                        <p className="font-bold text-lg w-40">
+                            {totalPrice.toFixed(2)} ₽
+                        </p>
+                    </div>
+                    <div className="mb-2 ml-20">
+                        <TextField
+                            label="Ваш TelegramID"
+                            variant="outlined"
+                            value={telegramFeed}
+                            onChange={handleTelegramIdChange}
+                            size="small"
+                        />
+                    </div>
                     <Button
-                        className="mb-3 mt-1 ml-10 min-w-[220px] h-30 bg-gray-600 hover:bg-gray-700 text-white"
+                        sx={{
+                            mb: 3,
+                            mt: 1,
+                            ml: 10,
+                            minWidth: 220,
+                            height: 30,
+                            backgroundColor: "#949494",
+                            "&:hover": {
+                                background: "#767676",
+                            },
+                        }}
                         variant="contained"
                         onClick={handleCheckout} // Здесь вызываем функцию оформления заказа
                     >
                         Оформить заказ
                     </Button>
-                )}
-            </div>
+                </div>
+            )}
         </div>
     );
 }
