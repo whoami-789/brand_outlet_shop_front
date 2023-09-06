85261396b9fc956db603f8c127538b97b2b87f17
tudya <tudya1238@gmail.com>
2023-09-06 10:20:25 +0500

    cart items

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Avatar, Box, Button } from "@mui/material";
+import axios from "axios";
 
 interface CartProps {
     product: {
@@ -22,15 +23,40 @@ interface CartProps {
 export function Cart({ product, productSize }: CartProps) {
     const [quantity, setQuantity] = useState(1);
 
-    const handleDecrease = () => {
-        if (quantity > 1) {
-            setQuantity(quantity - 1);
-        }
+    const sessionToken = localStorage.getItem("sessionToken");
+
+    const decreaseQuantity = () => {
+        // Отправить запрос на сервер для уменьшения количества товара
+        axios.post("https://brand-outlet.shop/api/cart/decrease", {
+            sessionId: sessionToken,
+            size: productSize.size,
+        })
+            .then((response) => {
+                if (quantity > 1) {
+                    setQuantity(quantity - 1);
+                }
+            })
+            .catch((error) => {
+                // Обработка ошибки
+                console.error("Ошибка при уменьшении количества товара:", error);
+            });
     };
 
-    const handleIncrease = () => {
-        setQuantity(quantity + 1);
+    const increaseQuantity = () => {
+        // Отправить запрос на сервер для увеличения количества товара
+        axios.post("https://brand-outlet.shop/api/cart/increase", {
+            sessionId: sessionToken,
+            size: productSize.size,
+        })
+            .then((response) => {
+                setQuantity(quantity + 1);
+            })
+            .catch((error) => {
+                // Обработка ошибки
+                console.error("Ошибка при увеличении количества товара:", error);
+            });
     };
+
 
     return (
         <>
@@ -64,7 +90,7 @@ export function Cart({ product, productSize }: CartProps) {
                         <div className="ml-4 w-48">
                             <p className="text-sm w-48 truncate">{product.title}</p>
                             <p className="font-bold text-sm w-40">Размер: {productSize.size}</p>
-                            <p className="font-bold text-sm w-40">{productSize.priceRub} ₽</p>
+                            <p className="font-bold text-sm w-40">{productSize.priceRub * quantity} ₽</p>
                             <Box
                                 sx={{
                                     borderRadius: 1.5,
@@ -75,7 +101,7 @@ export function Cart({ product, productSize }: CartProps) {
                                 }}
                             >
                                 <Button
-                                    onClick={handleDecrease}
+                                    onClick={decreaseQuantity}
                                     sx={{
                                         color: '#ffff',
                                         minWidth: 45,
@@ -92,7 +118,7 @@ export function Cart({ product, productSize }: CartProps) {
                                     {quantity}
                                 </p>
                                 <Button
-                                    onClick={handleIncrease}
+                                    onClick={increaseQuantity}
                                     sx={{
                                         color: '#ffff',
                                         minWidth: 45,
