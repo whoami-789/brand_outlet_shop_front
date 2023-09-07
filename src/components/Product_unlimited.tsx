c2bcb54ba8e622d52b2faa7e84f23ffb95e2cd59
tudya <tudya1238@gmail.com>
2023-09-07 16:50:17 +0500

    cart button

--- src/components/Product_unlimited.tsx
+++ src/components/Product_unlimited.tsx
@@ -183,27 +183,14 @@ export function Products_Unlimited({product}: ProductsProps) {
                     </div>
                     <div className="ml-4 z-1">
                         <div className="ml-2 mt-0 self-center w-full relative z-10">
-                            <Button
+                            <button
                                 onClick={addToCart}
-                                sx={{
-                                    mt: 1,
-                                    mb: 2,
-                                    minWidth: 120,
-                                    height: 30,
-                                    backgroundColor: isAddedToCart ? "#949494" : "#000000",
-                                    color: isAddedToCart ? "#ffffff" : "#ffffff",
-                                    transition: "background-color 0.3s, color 0.3s", // Анимация смены цвета
-                                    "&:hover": {
-                                        background: isAddedToCart ? "#767676" : "#949494",
-                                    },
-                                    animation: isAddedToCart
-                                        ? "buttonClick 1s"
-                                        : "none", // Анимация при клике
-                                }}
-                                variant="contained"
+                                className={`bg-black text-white px-4 py-2 rounded-full transition-transform transform ${
+                                    isAddedToCart ? "-translate-y-16 opacity-0" : "translate-y-0 opacity-100"
+                                } hover:bg-gray-600 hover:scale-105`}
                             >
                                 {isAddedToCart ? "В КОРЗИНЕ" : "В корзину"}
-                            </Button>
+                            </button>
                         </div>
                     </div>
                 </Box>
