de597554d2a3ebfb1adf99c3dc9f8bb0de66c9ac
tudya <tudya1238@gmail.com>
2023-09-07 23:28:02 +0500

    fix: create category

--- src/admin/CategoryForm.tsx
+++ src/admin/CategoryForm.tsx
@@ -38,7 +38,6 @@ function CategoryForm({ initialCategory, onSave, onCancel }: CategoryFormProps) 
                 <button
                     type="submit"
                     className="bg-blue-500 text-white px-4 py-2 rounded mr-2 hover:bg-blue-600"
-                    onClick={handleSubmit}
                 >
                     Сохранить
                 </button>
