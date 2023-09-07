59ff005451104176e89c4014c18e308fa9bb6812
tudya <tudya1238@gmail.com>
2023-09-07 12:53:01 +0500

    cart button

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -8,18 +8,16 @@ interface CartButtonProps {
 
 export function CartButton({ onClick, cartItemCount }: CartButtonProps) {
     return (
-        <div className="relative">
-            <button
-                className="fixed bottom-4 right-4 rounded-full bg-white p-2"
-                onClick={onClick}
-            >
-                <ShoppingCartIcon />
-            </button>
+        <button
+            className="fixed bottom-4 right-4 rounded-full bg-white p-2 relative"
+            onClick={onClick}
+        >
+            <ShoppingCartIcon />
             {cartItemCount > 0 && (
-                <div className="bg-red-500 text-white rounded-full w-6 h-6 text-center absolute bottom-2 right-2">
+                <div className="bg-red-500 text-white rounded-full w-6 h-6 text-center absolute -top-2 -right-2">
                     {cartItemCount}
                 </div>
             )}
-        </div>
+        </button>
     );
 }
